16929d54468c7e229e3ee01af3881143953cca0d
victorcuervo <vcuervo@gmail.com>
2024-01-05 22:03:46 +0100

    Fichero copia de posts

---
+++ copy-posts.sh
@@ -0,0 +1,28 @@
+echo "Copiando Java..."
+cp -R _data/Java ../w3api/_data/
+cp -R _posts/Java ../w3api/_posts/
+echo "Copiando HTML..."
+cp -R _data/HTML ../w3api/_data/
+cp -R _posts/HTML ../w3api/_posts/
+echo "Copiando HTMLDOM..."
+cp -R _posts/HTMLDOM ../w3api/_posts/
+echo "Copiando CSS..."
+cp -R _data/CSS ../w3api/_data/
+cp -R _posts/CSS ../w3api/_posts/
+echo "Copiando Javascript..."
+cp -R _data/Javascript ../w3api/_data/
+cp -R _posts/Javascript ../w3api/_posts/
+echo "Copiando DOM..."
+cp -R _posts/DOM ../w3api/_posts/
+echo "Copiando WebAPI..."
+cp -R _posts/WebAPI ../w3api/_posts/
+echo "Copiando Python..."
+cp -R _data/Python ../w3api/_data/
+cp -R _posts/Python ../w3api/_posts/
+echo "Copiando Blog..."
+cp -R _posts/blog ../w3api/_posts/
+cp -R assets/images/covers ../w3api/assets/images/
+
+
+echo "Copiando .htaccess"
+cp .htaccess ../w3api/.htaccess
